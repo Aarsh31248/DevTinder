38e5a492ac8df3a2824960c8612988577872cad8
Aarsh Singh <starfire9599@gmail.com>
2026-03-13 09:50:39 +0530

    Exploring Get, Post, Delete API calls on same route in postman

--- DevTinderBackend/src/app.js
+++ DevTinderBackend/src/app.js
@@ -6,9 +6,25 @@ app.use("/test", (req, res) => {
   res.send("I am Test!");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("I am Hello!");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Aarsh", lastName: "Singh" });
 });
+
+app.post("/user", (req, res) => {
+  res.send("Database updated successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted user successfully!");
+});
+
+// app.use("/hello/2", (req, res) => {
+//   res.send("I am Hello from 2!");
+// });
+
+// app.use("/hello", (req, res) => {
+//   res.send("I am Hello!");
+// });
 
 app.use("/", (req, res) => {
   res.send("I am Home Page!");
