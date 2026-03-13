2a3c182599790b1350614e219e599c4a169857db
Aarsh Singh <starfire9599@gmail.com>
2026-03-13 10:14:11 +0530

    Exploring parameters and queries

--- DevTinderBackend/src/app.js
+++ DevTinderBackend/src/app.js
@@ -2,21 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("I am Test!");
-});
-
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:phone", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Aarsh", lastName: "Singh" });
 });
 
-app.post("/user", (req, res) => {
-  res.send("Database updated successfully!");
+app.get("/test", (req, res) => {
+  console.log(req.query);
+  res.send({ firstName: "Aarsh", lastName: "Singh" });
 });
 
-app.delete("/user", (req, res) => {
-  res.send("Deleted user successfully!");
-});
+// app.use("/test", (req, res) => {
+//   res.send("I am Test!");
+// });
+
+// app.get("/user", (req, res) => {
+//   res.send({ firstName: "Aarsh", lastName: "Singh" });
+// });
+
+// app.post("/user", (req, res) => {
+//   res.send("Database updated successfully!");
+// });
+
+// app.delete("/user", (req, res) => {
+//   res.send("Deleted user successfully!");
+// });
 
 // app.use("/hello/2", (req, res) => {
 //   res.send("I am Hello from 2!");
@@ -26,9 +36,9 @@ app.delete("/user", (req, res) => {
 //   res.send("I am Hello!");
 // });
 
-app.use("/", (req, res) => {
-  res.send("I am Home Page!");
-});
+// app.use("/", (req, res) => {
+//   res.send("I am Home Page!");
+// });
 
 app.listen(3000, () => {
   console.log("Server is running on port - 3000...");
