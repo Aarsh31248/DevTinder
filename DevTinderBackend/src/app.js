bb50b4a2681eb71b4173b99e1d8a44e0543ecca5
Aarsh Singh <starfire9599@gmail.com>
2026-03-13 22:37:39 +0530

    Created user schema and model. Check with creating the instance of model

--- DevTinderBackend/src/app.js
+++ DevTinderBackend/src/app.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
+
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Virat",
+    lastName: "Kohli",
+    emailId: "viratkohli@123",
+    password: "Virat@123",
+  });
+
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong", err.message);
+  }
+});
 
 connectDB()
   .then(() => {
@@ -12,4 +30,3 @@ connectDB()
   .catch((err) => {
     console.log("Databse cannot be Connected!!");
   });
- 
