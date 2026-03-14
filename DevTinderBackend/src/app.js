e4687841f034d419e2f340d03b58c11ddf3b2092
Aarsh Singh <starfire9599@gmail.com>
2026-03-14 19:12:24 +0530

    Dynamic data in signup api

--- DevTinderBackend/src/app.js
+++ DevTinderBackend/src/app.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
-
 const app = express();
 
-app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Virat",
-    lastName: "Kohli",
-    emailId: "viratkohli@123",
-    password: "Virat@123",
-  });
+app.use(express.json());
 
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  
   try {
     await user.save();
     res.send("User added successfully");
