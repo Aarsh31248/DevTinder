0e5fa8c8420f6948cb3a19ee26ed641b54b6b62a
Aarsh Singh <starfire9599@gmail.com>
2026-03-16 21:36:01 +0530

    Created api for getting all requests of interested users

---
+++ DevTinderBackend/src/routes/user.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const userRouter = express.Router();
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequest = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", "firstName lastName photoUrl about skills");
+
+    res.json({ message: "Data fetched Successfully", data: connectionRequest });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+module.exports = userRouter;
