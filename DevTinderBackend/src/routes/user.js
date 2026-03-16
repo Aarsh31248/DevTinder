87ff482444b5f5bc4d0be1daba70577f3ac57a96
Aarsh Singh <starfire9599@gmail.com>
2026-03-16 22:34:46 +0530

    Created api for getting all the connections

--- DevTinderBackend/src/routes/user.js
+++ DevTinderBackend/src/routes/user.js
@@ -3,6 +3,8 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = "firstName lastName photoUrl about skills";
+
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -10,9 +12,35 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequest = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", "firstName lastName photoUrl about skills");
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     res.json({ message: "Data fetched Successfully", data: connectionRequest });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequest.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) { // we cant compare mongodb id
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ data });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
