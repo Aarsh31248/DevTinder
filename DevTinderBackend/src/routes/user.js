e7ecd2598c5181c54b55c47675383af081778f5b
Aarsh Singh <starfire9599@gmail.com>
2026-03-16 23:56:50 +0530

    Created feed API

--- DevTinderBackend/src/routes/user.js
+++ DevTinderBackend/src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl about skills";
 
@@ -34,13 +35,42 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       .populate("toUserId", USER_SAFE_DATA);
 
     const data = connectionRequest.map((row) => {
-      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) { // we cant compare mongodb id
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        // we cant compare mongodb id
         return row.toUserId;
       }
       return row.fromUserId;
     });
 
     res.json({ data });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA);
+
+    res.send(users);
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
