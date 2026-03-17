fb22ac10301f904a7f56ad7180e56a0cc86f98c7
Aarsh Singh <starfire9599@gmail.com>
2026-03-17 12:54:53 +0530

    Created Routes using react router dom

--- DevTinderFrontend/src/App.jsx
+++ DevTinderFrontend/src/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import Navbar from "./Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
