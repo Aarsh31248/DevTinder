bd9c9f3f220118864d04255a286a063775fa8503
Aarsh Singh <starfire9599@gmail.com>
2026-03-19 23:46:58 +0530

    Get the feed of user and put into the store

--- DevTinderFrontend/src/components/Feed.jsx
+++ DevTinderFrontend/src/components/Feed.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { BASE_URL } from "../utils/constant";
+import { useEffect } from "react";
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default Feed
+  const getFeed = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data));
+    } catch (err) {
+      console.log("ERROR:", err.response?.data);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return <div></div>;
+};
+
+export default Feed;
